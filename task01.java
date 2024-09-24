083f21b09dfe1ceb7d683238857f33c051235733
ceyhun <sahancn@mail.uc.edu>
2024-09-24 14:27:33 -0400

    Init

---
+++ task01.java
@@ -0,0 +1,12 @@
+public class task01 {
+    public static void main(String[] args) {
+        double itemPrice = 10;
+        double salesTax = 0.05;
+
+        itemPrice = itemPrice * salesTax;
+
+        System.out.println("your item price after the sales tax is " + itemPrice);
+    }
+
+
+}
